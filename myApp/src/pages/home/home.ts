117d0551941ef86f3258d6aa457be4daa7b83639
pauljohnleonard <pauljohnleonard@gmail.com>
2018-06-11 17:40:30 +0100

    WIP

--- myApp/src/pages/home/home.ts
+++ myApp/src/pages/home/home.ts
@@ -7,6 +7,18 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+
+  days=
+    [{label:"mon",time:"07:45",on:true},
+    {label:"tue",time:"07:45",on:true},
+    {label:"wed",time:"07:45",on:true},
+    {label:"thur",time:"07:45",on:true},
+    {label:"fri",time:"07:45",on:true},
+    {label:"sat",time:"10:45",on:true},
+    {label:"sun",time:"12:00",on:false}]
+
+  
+
   constructor(public navCtrl: NavController) {
 
   }
--- myApp/src/providers/ip/ip.ts
+++ myApp/src/providers/ip/ip.ts
@@ -10,44 +10,44 @@ import { Subject} from "rxjs/Subject"
 
 
 
-function getLanIP(cb) {
-  // NOTE: window.RTCPeerConnection is "not a constructor" in FF22/23
-   var RTCPeerConnection = /*window.RTCPeerConnection ||*/ window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
+// function getLanIP(cb) {
+//   // NOTE: window.RTCPeerConnection is "not a constructor" in FF22/23
+//    var RTCPeerConnection = /*window.RTCPeerConnection ||*/ window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
 
-    var rtc = new RTCPeerConnection({iceServers:[]});
+//     var rtc = new RTCPeerConnection({iceServers:[]});
 
-    if (1 || window.mozRTCPeerConnection) {      // FF [and now Chrome!] needs a channel/stream to proceed
-        rtc.createDataChannel('', {reliable:false});
-    };
+//     if (1 || window.mozRTCPeerConnection) {      // FF [and now Chrome!] needs a channel/stream to proceed
+//         rtc.createDataChannel('', {reliable:false});
+//     };
     
-    rtc.onicecandidate = function (evt) {
-        // convert the candidate to SDP so we can run it through our general parser
-        // see https://twitter.com/lancestout/status/525796175425720320 for details
-        if (evt.candidate) grepSDP("a="+evt.candidate.candidate);
-    };
+//     rtc.onicecandidate = function (evt) {
+//         // convert the candidate to SDP so we can run it through our general parser
+//         // see https://twitter.com/lancestout/status/525796175425720320 for details
+//         if (evt.candidate) grepSDP("a="+evt.candidate.candidate);
+//     };
 
-    rtc.createOffer(function (offerDesc) {
-        grepSDP(offerDesc.sdp);
-        rtc.setLocalDescription(offerDesc);
-    }, function (e) { console.warn("offer failed", e); });
+//     rtc.createOffer(function (offerDesc) {
+//         grepSDP(offerDesc.sdp);
+//         rtc.setLocalDescription(offerDesc);
+//     }, function (e) { console.warn("offer failed", e); });
       
-    function grepSDP(sdp) {
-      //  var hosts = [];
-        sdp.split('\r\n').forEach(function (line) { // c.f. http://tools.ietf.org/html/rfc4566#page-39
-            if (~line.indexOf("a=candidate")) {     // http://tools.ietf.org/html/rfc4566#section-5.13
-                let parts = line.split(' '),        // http://tools.ietf.org/html/rfc5245#section-15.1
-                    addr = parts[4],
-                    type = parts[7];
-                if (type === 'host') cb(addr);
-            } else if (~line.indexOf("c=")) {       // http://tools.ietf.org/html/rfc4566#section-5.7
-                let parts = line.split(' '),
-                    addr = parts[2];
-                cb(addr);
-            }
-        });
-    }
+//     function grepSDP(sdp) {
+//       //  var hosts = [];
+//         sdp.split('\r\n').forEach(function (line) { // c.f. http://tools.ietf.org/html/rfc4566#page-39
+//             if (~line.indexOf("a=candidate")) {     // http://tools.ietf.org/html/rfc4566#section-5.13
+//                 let parts = line.split(' '),        // http://tools.ietf.org/html/rfc5245#section-15.1
+//                     addr = parts[4],
+//                     type = parts[7];
+//                 if (type === 'host') cb(addr);
+//             } else if (~line.indexOf("c=")) {       // http://tools.ietf.org/html/rfc4566#section-5.7
+//                 let parts = line.split(' '),
+//                     addr = parts[2];
+//                 cb(addr);
+//             }
+//         });
+//     }
 
-}
+// }
 
 @Injectable()
 export class IpProvider {
@@ -55,10 +55,10 @@ export class IpProvider {
   ipSubject= new Subject<any>(); 
   constructor(public http: HttpClient) {
 
-    getLanIP( (val) => {
-      this.ipSubject.next({ip:val});
-    });
-    console.log('Hello IpProvider Provider');
+    // getLanIP( (val) => {
+    //   this.ipSubject.next({ip:val});
+    // });
+     console.log('Hello IpProvider Provider');
   }
 
 }
